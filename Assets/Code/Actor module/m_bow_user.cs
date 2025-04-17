ebf04e5379f16bac64bf834f4a4224d82a7c8104
SartinPixel <96411452+SartinPixel@users.noreply.github.com>
2025-04-17 18:58:10 +0300

    setting bow user

--- Assets/Code/Actor module/m_bow_user.cs
+++ Assets/Code/Actor module/m_bow_user.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Pixify;
 using UnityEngine;
 
 namespace Triheroes.Code
@@ -7,9 +8,15 @@ namespace Triheroes.Code
     public class m_bow_user : m_weapon_user_standard<Bow>
     {
         protected override int HandIndex => 0;
-
         protected override int AniLayer => ms.bow;
-
         protected override Quaternion DefaultRotation => Const.BowDefaultRotation;
+
+        public Vector3 RotY;
+        public SuperKey state = StateKey.zero;
+
+        protected override void OnAquire1()
+        {
+            state = StateKey.zero;
+        }
     }
 }
