fa15119f679213774fbc259a90ce854a70198373
SartinPixel <96411452+SartinPixel@users.noreply.github.com>
2025-04-17 11:10:57 +0300

    player drawing weapon test

--- Assets/Code/PlayerController/pc_draw.cs
+++ Assets/Code/PlayerController/pc_draw.cs
@@ -7,14 +7,20 @@ namespace Triheroes.Code
 {
     public class pc_draw : action
     {
+        [Depend]
+        m_equip me;
+
+        protected override void BeginStep()
+        {
+            
+        }
 
         protected override bool Step()
         {
-            //if (Player.GetButtonDown (BoutonId.A))
-            //TryADrawWeapon ();
+           if (Player.GetButtonDown (BoutonId.A))
+            me.DrawWeapon ( 0 );
 
             return false;
         }
-
     }
 }
