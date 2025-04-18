f782ea2e0144af685f850524ef299bcea3263f91
SartinPixel <96411452+SartinPixel@users.noreply.github.com>
2025-04-18 09:40:43 +0300

    set pc_bow

---
+++ Assets/Code/PlayerController/pc_bow.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Pixify;
+using UnityEngine;
+
+namespace Triheroes.Code
+{
+    public class pc_bow : action
+    {
+        protected override bool Step()
+        {
+            return base.Step();
+        }
+    }
+}
