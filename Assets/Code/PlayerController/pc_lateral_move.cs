8b8c066c9fda5ceeded804a84c5cb296e742f27c
SartinPixel <96411452+SartinPixel@users.noreply.github.com>
2025-04-18 11:30:22 +0300

    added pc_lateral_move

---
+++ Assets/Code/PlayerController/pc_lateral_move.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Pixify;
+using UnityEngine;
+
+namespace Triheroes.Code
+{
+    public class pc_lateral_move : action
+    {
+        [Depend]
+        pm_master_controller pmc;
+        [Depend]
+        m_standard_character_controller_host mscch;
+        [Depend]
+        m_ground_data mgd;
+
+        protected override void BeginStep()
+        {
+            pmc.SetDefaultMaster (this, mscch.ss);
+        }
+
+        protected override bool Step()
+        {
+            Vector3 InputAxis;
+            InputAxis = Player.GetAxis3();
+            InputAxis = Vecteur.LDir (new Vector3(0, MainCamera.o.RotY.y, 0),InputAxis) * 6f;
+
+            if (mgd.onGround)
+            mscch.agm.WalkLateral ( InputAxis );
+            else
+            mscch.af.MoveAir ( InputAxis );
+
+            return false;
+        }
+    }
+}
