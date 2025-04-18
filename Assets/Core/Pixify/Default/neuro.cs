2242f6ba632b3f80b0aa64724135327ee88888f4
SartinPixel <96411452+SartinPixel@users.noreply.github.com>
2025-04-18 18:25:17 +0300

    added spawner

--- Assets/Core/Pixify/Default/neuro.cs
+++ Assets/Core/Pixify/Default/neuro.cs
@@ -17,10 +17,11 @@ namespace Pixify
             this.o = o;
         }
 
-        public void set (bool repeat = true, bool reset = true)
+        public sequence set (bool repeat = true, bool reset = true)
         {
             this.repeat = repeat;
             this.reset = reset;
+            return this;
         }
 
         protected sealed override void BeginStep()
@@ -67,9 +68,10 @@ namespace Pixify
             this.o = o;
         }
 
-        public void Set (bool StopWhenFirstNodeStopped)
+        public parallel Set (bool StopWhenFirstNodeStopped)
         {
             this.StopWhenFirstNodeStopped = StopWhenFirstNodeStopped;
+            return this;
         }
 
         protected override void BeginStep()
@@ -121,4 +123,77 @@ namespace Pixify
             }
         }
     }
+
+    
+    public class spawner : neuro
+    {
+        public bool IntervalFirst;
+        public int SpamCount = 1;
+        public float TimeInterval = 1;
+
+        int spamCounter;
+        float time;
+        bool isRunning;
+
+        public spawner ( params action[] o)
+        {
+            this.o = o;
+        }
+
+        public spawner Set ( bool intervalFirst, int spamCount, float TimeIntervalSec )
+        {
+            IntervalFirst = intervalFirst;
+            SpamCount = spamCount;
+            TimeInterval = TimeIntervalSec;
+            return this;
+        } 
+
+        protected override void BeginStep()
+        {
+            spamCounter = 0;
+            time = 0;
+            if (!IntervalFirst)
+            {
+                isRunning = true;
+                o[0].iStart();
+            }
+        }
+
+        protected override bool Step()
+        {
+            if (isRunning)
+            {
+                if (!o[0].on)
+                {
+                    isRunning = false;  // Stop running and start the timer
+                    time = 0f;
+                    spamCounter++;
+                    if (spamCounter >= SpamCount)
+                        return true;  // Stop the node if spam count is reached
+                }
+                else
+                {
+                    o[0].iExecute();  // Continue updating the child if it's still running
+                }
+            }
+            else
+            {
+                time += Time.deltaTime;
+                if (time >= TimeInterval)
+                {
+                    isRunning = true;
+                    o[0].iStart();  // Restart the child after the interval
+                }
+            }
+
+            return false;  // Node is not yet finished
+        }
+
+        protected override void Abort()
+        {
+            if (o[0].on)
+                o[0].iAbort();
+        }
+    }
+
 }
