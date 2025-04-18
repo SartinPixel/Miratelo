8d91f859a66d6eaec48100c2e737226a4af89f37
SartinPixel <96411452+SartinPixel@users.noreply.github.com>
2025-04-18 11:29:54 +0300

    added skin_produceral_bow

--- Assets/Code/Game Main/TriheroesMighty.cs
+++ Assets/Code/Game Main/TriheroesMighty.cs
@@ -11,14 +11,21 @@ namespace Triheroes.Code
         {
             Systems = new List<CoreSystemBase>()
             {
+                // character physic data
                 new s_capsule_character_controller_ground_data (),
 
+                // procedural animation
+                new s_skin_produceral_bow (),
+
+                // character controller and AI
                 new s_character_controller (),
                 new s_state_player (),
 
+                // character physic
                 new s_ccc_gravity (),
-
                 new s_move (),
+
+                // character graphic and animation event
                 new s_skin ()
             };
         }
