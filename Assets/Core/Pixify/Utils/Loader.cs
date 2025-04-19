1076a0ad9d1eeb0618ab2b2d0935854664566f0b
SartinPixel <96411452+SartinPixel@users.noreply.github.com>
2025-04-19 15:29:47 +0300

    added loader utility

---
+++ Assets/Core/Pixify/Utils/Loader.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace Pixify
+{
+    /// <summary>
+    /// this attribute will let field to be serialized in paper // must be public
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field)]
+    public class ExportAttribute : Attribute { }
+
+    public static class Loader
+    {
+        public enum Comparator { less, more, equal, lessEqual, moreEqual }
+        
+        public static T CopyWithExport <T> ( T a )
+        {
+            T b = (T) Activator.CreateInstance (a.GetType());
+            foreach ( FieldInfo fi in a.GetType().GetFields() )
+            {
+                if (fi.IsPublic && fi.GetCustomAttribute<ExportAttribute>() != null)
+                fi.SetValue ( b ,fi.GetValue ( a ) );
+            }
+            return b;
+        }
+
+        public static T LoadIntoScene <T> ( string path ) where T: UnityEngine.Object
+        {
+            T o = Resources.Load <T> (path);
+
+            if (!o)
+            {
+            Debug.LogError ( string.Concat ("hardcoded Resources not existing in ",path) );
+            return null;
+            }
+            else
+            return ScriptableObject.Instantiate ( o );
+        }
+
+        public static bool Compare (  float A, float B, Comparator comparator)
+        {
+            switch (comparator)
+            {
+                case Comparator.less:
+                return A<B;
+                case Comparator.more:
+                return A>B;
+                case Comparator.equal:
+                return A==B;
+                case Comparator.lessEqual:
+                return A<=B;
+                case Comparator.moreEqual:
+                return A>=B;
+            }
+            return false;
+        }
+    }
+}
