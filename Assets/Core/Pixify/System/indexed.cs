20a4e96992d798ae36de23e4658a40235029ca23
SartinPixel <96411452+SartinPixel@users.noreply.github.com>
2025-04-19 17:48:36 +0300

    added indexed module
    To make some module possible to get by using gameObject instance id

---
+++ Assets/Core/Pixify/System/indexed.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pixify
+{
+    public abstract class indexedmodule<T> : module where T : indexedmodule<T>, new()
+    {
+        public static Dictionary<int, T> index;
+
+        /// <summary>
+        /// to be initialized by sceneMaster
+        /// </summary>
+        public static void InitIndex()
+        {
+            index = new Dictionary<int, T>();
+        }
+
+        public sealed override void Create()
+        {
+            index.Add(character.gameObject.GetInstanceID(), this as T);
+        }
+
+        public virtual void Create1 () {}
+    }
+
+    public abstract class indexedcore<T> : core where T : indexedcore<T>, new()
+    {
+        public static Dictionary<int, T> index;
+
+        /// <summary>
+        /// to be initialized by sceneMaster
+        /// </summary>
+        public static void InitIndex()
+        {
+            index = new Dictionary<int, T>();
+        }
+
+        protected sealed override void OnAquire()
+        {
+            index.Add(character.gameObject.GetInstanceID(), this as T);
+            OnAquire1();
+        }
+        protected virtual void OnAquire1() { }
+        protected virtual void OnFree1() { }
+
+        protected sealed override void OnFree()
+        {
+            index.Remove(character.gameObject.GetInstanceID());
+            OnFree1();
+        }
+    }
+}
